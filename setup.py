63922869d257c26b9fb694263538951d33867990
Adrien Champion <adrien.champion@email.com>
2015-03-17 20:07:26 -0500

    Setup does cp readme for distutils and rm after setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import shutil
+from src.io import warning, info
 
-setup(
+readme_md = "README.md"
+readme_target = "README"
+should_rm_readme_target = False
+
+print("")
+
+# Copy `readme_md` to `readme_target` if the former exists but the
+# former does not.
+if os.path.isfile(readme_md) :
+
+    if not os.path.isfile(readme_target) :
+        info(
+            "copying \"" + readme_md + "\" to \"" + readme_target + \
+            "\" for distutils."
+        )
+        shutil.copyfile( readme_md, readme_target )
+        should_rm_readme_target = True
+
+    else : warning(
+        "distutils readme file \"" + readme_target + \
+        "\" detected, not copying \"" + readme_md + \
+        "\"."
+    )
+
+else : warning( "readme file \"" + readme_md + "\" not found." )
+
+print("")
+
+# Setup things.
+try: setup(
     name='testeas',
     version='0.0',
     description='Test Execution and Analysis System',
@@ -13,3 +45,13 @@ setup(
     package_dir={'testeas': 'src'},
     packages=['testeas'],
 )
+
+finally :
+    print("")
+
+    # Whatever happens during setup, remove target readme if necessary.
+    if should_rm_readme_target:
+        info("deleting distutils readme file \"" + readme_target + "\".")
+        os.remove( readme_target )
+
+    print("")
